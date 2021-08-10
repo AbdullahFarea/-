77e9087b750e36beff0b0b8c1220c01bd61b5f7e
Abdullah Farea <87715865+AbdullahFarea@users.noreply.github.com>
2021-08-10 17:40:42 +0300

    added new feature

--- js/javascript.js
+++ js/javascript.js
@@ -5,15 +5,62 @@ const alertContainer = document.getElementsByClassName('alert-container')[0]
 const alertDisplay = document.getElementsByClassName('alert')[0]
 const yes = document.getElementsByClassName('yes')[0]
 const no = document.getElementsByClassName('no')[0]
+const clickSound = new Audio('./sounds/click.mp3')
+const soundIcon = document.getElementById('soundIcon')
+const soundBtn = document.querySelector('.sound')
+let SoundPerference = localStorage.getItem('sound')
+
+function Sound() {
+    SoundPerference = localStorage.getItem('sound')
+    if (SoundPerference == "vibrate") return navigator.vibrate(130)
+    if (SoundPerference == "none") return "";
+    return clickSound.play()
+}
+
+function changeIcon() {
+    SoundPerference = localStorage.getItem('sound')
+    if (SoundPerference == "vibrate") {
+        soundIcon.classList.remove('fa-volume-up')
+        soundIcon.classList.remove('fa-volume-mute')
+        return soundIcon.classList.add('fa-wave-square')
+    }
+     if (SoundPerference == "none") {
+        soundIcon.classList.remove('fa-volume-up')
+        soundIcon.classList.remove('fa-wave-square')
+        return soundIcon.classList.add('fa-volume-mute')
+    }
+    soundIcon.classList.remove('fa-volume-mute')
+    soundIcon.classList.remove('fa-wave-square')
+    return soundIcon.classList.add('fa-volume-up')
+}
+
+changeIcon()
+
+function changeSound() {
+    if (SoundPerference == "vibrate") {
+        localStorage.setItem("sound", "none")
+    }else if(SoundPerference == "none") {
+        localStorage.setItem("sound", "click")
+    }else {
+        localStorage.setItem("sound", "vibrate")
+    }
+    SoundPerference = localStorage.getItem('sound')
+    changeIcon()
+}
+
+soundBtn.addEventListener("click", () => {
+    changeSound();
+    Sound()
+})
 
 for(let i = 0; i < counterDisplay.length; i++) {
+    counterDisplay[i].textContent = counterDisplay[i].dataset.start
 
     increase[i].addEventListener('click', () => {
         let num = counterDisplay[i].textContent
         if(num != 0) {
             counterDisplay[i].textContent =  parseInt(num) - 1
-            const click = new Audio('./sounds/click.mp3')
-            click.play()
+            Sound()
         }
     })
 
